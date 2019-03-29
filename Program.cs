e2912c5fe77df76ab0a74a1681ef514a14c21378
Carlos Lantigua <clantigua954@gmail.com>
2019-03-28 23:24:55 -0400

    making game more dynamic

--- Program.cs
+++ Program.cs
@@ -23,6 +23,48 @@ namespace Projects
             string username = Console.ReadLine();
             // user users name
             Console.WriteLine("Hello {0}, let's play a game...", username);
+            // Init correct number
+            // int correctNumber = 7;
+
+            // Create new random object
+            Random random = new Random();
+            int correctNumber = random.Next(1, 11);
+
+            // Init guess var
+            int guess = 0;
+            // Ask the user to guess a number
+            Console.WriteLine("Guess a number between 1 and 10");
+            // while guess is not correct
+            while (guess != correctNumber)
+            {
+                // Get users input
+                string userInput = Console.ReadLine();
+                // make sure input is a number
+                if (!int.TryParse(userInput, out guess))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("{0}, that's not even a number...", username);
+                    Console.ResetColor();
+
+                    // keep it going
+                    continue;
+                }
+                // Cast to int and put into guess var
+                guess = Int32.Parse(userInput);
+                // Match guess to correct number
+                if (guess != correctNumber)
+                {
+                    // change the color, output the message then change it back
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Wrong number {0}, please try again...", username);
+                    Console.ResetColor();
+                }
+
+            }
+            // Output success message
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Great job {0}!! The correct number was {1}!!...", username, correctNumber);
+            Console.ResetColor();
         }
     }
 }
